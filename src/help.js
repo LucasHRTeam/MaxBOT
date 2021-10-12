4dbd29920b97e5dc16e2f3118415a1e1f7becd92
Lucas Santos Da Hora <89528999+LucasHRTeam@users.noreply.github.com>
2021-10-12 01:19:08 +0000

    update help.js

--- src/help.js
+++ src/help.js
@@ -78,6 +78,69 @@ _[mostra a funções ativas no grupo]_
 ➻ *${prefix}attp6*
 ━┉┉┉┉┉━━━━━━━━━━
 ╭┈──────⎆
+╰─➤【 *ADMINISTRAÇÃO* 】
+
+➻*${prefix}resetlink*
+➻*${prefix}anagrama*
+➻*${prefix}abrirgp*
+➻*${prefix}fechargp*
+➻*${prefix}welcome*
+➻*${prefix}leveling*
+➻*${prefix}nsfw*
+➻*${prefix}autostickerimg*
+➻*${prefix}onlytag*
+➻*${prefix}promote*
+➻*${prefix}demote*
+➻*${prefix}delete*
+➻*${prefix}destrava*
+➻*${prefix}add*
+➻*${prefix}kick*
+➻*${prefix}ban*
+➻*${prefix}hidemarcar*
+➻*${prefix}setdesc*
+➻*${prefix}setname*
+➻*${prefix}linkgroup*
+➻*${prefix}fotogp*
+━┉┉┉┉┉━━━━━━━━━━
+╭┈──────⎆
+╰─➤【 *OUTROS* 】
+
+➻*${prefix}infogp*
+➻*${prefix}ownergp*
+➻*${prefix}wiki*
+➻*${prefix}gimage*
+➻*${prefix}nsfwloli*
+➻*${prefix}loli*
+➻*${prefix}listonline*
+➻*${prefix}listpalavras
+➻*${prefix}listadmins*
+➻*${prefix}tagall*
+➻*${prefix}tagall3*
+➻*${prefix}carioca*
+➻*${prefix}raposo*
+➻*${prefix}going*
+➻*${prefix}checkprem*
+➻*${prefix}vip*
+
+╭┈──────⎆
+╰─➤【 *RANKINGS* 】
+
+➻*${prefix}rankgado*
+➻*${prefix}ranklindo*
+➻*${prefix}rankgay*
+➻*${prefix}rankcorno*
+➻*${prefix}all*
+➻*${prefix}gay*
+➻*${prefix}corno*
+➻*${prefix}gado*
+➻*${prefix}gostoso*
+➻*${prefix}cheguei*
+➻*${prefix}botfdp*
+➻*${prefix}botgay*
+➻*${prefix}botbaiano*
+➻*${prefix}botputa*
+➻*${prefix}botfalido*
+╭┈──────⎆
 ╰─➤【 *VOZ MODS* 】
 
 ➻ *${prefix}estourar*
@@ -113,7 +176,7 @@ _[mostra a funções ativas no grupo]_
 ➻ *${prefix}boton*
 ━┉┉┉┉┉━━━━━━━━━━
 ╭┈──────⎆
-╰─➤【 *OUTROS* 】
+╰─➤【 *SEARCH* 】
 
 ➻ *${prefix}ack*
 ➻ *${prefix}pornhub*
